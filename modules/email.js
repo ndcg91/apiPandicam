33fc3f23b7f6ca060d47c04fcce6152949e542d1
noel carcases <noel carcases>
2016-06-30 14:34:46 +0200

    registration problem

--- modules/email.js
+++ modules/email.js
@@ -48,7 +48,7 @@ exports.registered = function(res,user){
     from:'PandicamProject <info@pandicamproject.com>',
     to:user.email,
     subject: 'Bienvenido a Pandicam!',
-    html:ejs.render(registered,user.username)
+    html:ejs.render(registered,{usuario:user.username})
   }
   sendEmail(res,mailOptions,true);
 }
